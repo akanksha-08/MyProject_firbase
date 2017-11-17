1c4bf3b0763964cf826f11123320a4c17e5a65a3
akanksha-08 <32834313+akanksha-08@users.noreply.github.com>
2017-11-17 11:12:23 +0530

    ChatFirebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { AuthService } from './auth.service'; 
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+   title = 'Firebase Login';
+  /*timestamp: string;
+  messages: FirebaseListObservable<any[]>;
+ 
+  constructor(public authService: AuthService, public db: AngularFireDatabase) {}
+
+   ngOnInit() {
+      this.getChatData();
+  }
+   getChatData(){
+        this.messages = this.db.list('chat_messages');
+   }
+  newMessage(message) {
+   var msg = {
+        msg: message,
+        timestamp: Date.now(),
+        displayEmail: this.displayEmail,
+        //displayName: this.displayName
+   };
+    this.messages.push(message);
+  }*/
 }
+
